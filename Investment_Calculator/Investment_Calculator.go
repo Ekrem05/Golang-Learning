a5858aae00374b15a7a2084428fade0c1242cc34
Ekrem Beytula <ekremnptg@gmail.com>
2024-07-29 20:02:03 +0300

    Functions that return two values

--- Investment_Calculator/Investment_Calculator.go
+++ Investment_Calculator/Investment_Calculator.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"math"
 )
-
+const inflationRate = 2.5;
 func main() {
 	fmt.Println("Invest!!")
-	const inflationRate = 2.5;
+	
 	var investmentAmount float64;
 	var expectedReturn float64 ;
 	var years float64;
@@ -20,8 +20,7 @@ func main() {
 	fmt.Print("Years:")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount* math.Pow(1+expectedReturn/100,years)
-	futureRealValue := futureValue/ math.Pow(1+inflationRate/100,years)
+	futureValue, futureRealValue:=calculateFutureValues(investmentAmount,expectedReturn,years)
 	foramttedFv:=fmt.Sprintf("Future value without inflation: %.1f \n",futureValue)
 	fmt.Print(foramttedFv)
 	fmt.Printf("Future value with inflation: %.2f ",futureRealValue)
@@ -30,4 +29,10 @@ func main() {
 
 func customPrint(text string){
 	fmt.Println(text)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturn, years float64)(float64,float64){
+	futureValue := investmentAmount* math.Pow(1+expectedReturn/100,years);
+	futureRealValue := futureValue/ math.Pow(1+inflationRate/100,years);
+	return futureValue, futureRealValue;
 }
