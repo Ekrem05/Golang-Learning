102220955229b2a91744f7cc34e94017d62f8df0
Ekrem Beytula <ekremnptg@gmail.com>
2024-07-29 20:52:26 +0300

    Loops and conditionals

---
+++ Bank/bank.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func main() {
+	var balance float64 = 10000; 
+	for {
+		fmt.Println("Welcome to GO Lang");
+		fmt.Println("What do you want to do?")
+		fmt.Println("1. Check balance")
+		fmt.Println("2. Deposit money")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit")
+		fmt.Print("Please choose an action: ")
+		var choice int
+		fmt.Scan(&choice)
+	
+		if choice == 1{
+			fmt.Println("Your balance is: ",balance)
+		} else if choice == 2 {
+			fmt.Print("Amount: ")
+			var depositAmount float64;
+			fmt.Scan(&depositAmount)
+			if depositAmount<=0{
+				fmt.Println("Invalid amount! ")
+				continue;
+			}
+			balance += depositAmount;
+			fmt.Printf("Current balance: %.2f \n", balance)
+		} else if choice == 3 {
+			fmt.Print("Amount: ")
+			var withdrawAmount float64;
+			fmt.Scan(&withdrawAmount)
+			if withdrawAmount<=0{
+				fmt.Println("Invalid amount! ")
+				continue;
+			}
+			if withdrawAmount > balance{
+				fmt.Println("Invalid amount! You can't withdraw more than you have!")
+				continue;
+			}
+			balance -= withdrawAmount;
+			fmt.Printf("Current balance: %.2f \n", balance)
+		} else{
+			fmt.Print("Goodbye!")
+			break;
+		}
+	}
+	
+
+	
+
+}
---
+++ Bank/go.mod
@@ -0,0 +1,3 @@
+module bank.com
+
+go 1.22.5
