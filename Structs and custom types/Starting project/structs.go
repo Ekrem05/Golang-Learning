a950b261dd6aff69c731a2713588be9427235021
Ekrem Beytula <ekremnptg@gmail.com>
2024-08-05 21:30:27 +0300

    Export a struct

--- Structs and custom types/Starting project/structs.go
+++ Structs and custom types/Starting project/structs.go
@@ -1,52 +1,25 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
+
+	"example.com/structs/user"
 )
 
-type User struct {
-	firstName string
-	lastName string
-	birthdate string
-	createdAt time.Time
-}
-
-func (user User) outputUserDetails(){
-	fmt.Println(user.firstName,user.lastName,user.birthdate,user.createdAt)
-}
-//the reciever is just like a normal parameter
-func (user *User) clearUsername(){
-	user.firstName=""
-	user.lastName=""
-}
-
-func newUser(firstName,lastName,birthdate string) (*User,error) {
-	if firstName == "" || lastName =="" || birthdate == ""{
-		return nil, errors.New("all parameters are required");
-	}
-	return &User{
-		firstName,
-		lastName,
-		birthdate,
-		time.Now(),
-	},nil
-}
 
 func main() {
 	firstName := getUserData("Please enter your first name: ")
 	lastName := getUserData("Please enter your last name: ")
 	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
-	var user *User
+	var appUser *user.User
 
-	user,err := newUser(firstName,lastName,birthdate)
+	appUser,err := user.New(firstName,lastName,birthdate)
 	if err !=nil{
 		fmt.Println(err)
 	}
-	user.outputUserDetails()
-	user.clearUsername()
-	user.outputUserDetails()
+	appUser.OutputUserDetails()
+	appUser.ClearUsername()
+	appUser.OutputUserDetails()
 }
 
 func getUserData(promptText string) string {
