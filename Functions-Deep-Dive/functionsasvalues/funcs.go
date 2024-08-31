ac77eb86ab31f33d4530c4651fd513240bec9bed
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-08-31 14:32:03 +0300

    anonymous functions

--- Functions-Deep-Dive/functionsasvalues/funcs.go
+++ Functions-Deep-Dive/functionsasvalues/funcs.go
@@ -1,10 +1,14 @@
-package main
+package functionsarevalues
 
 import "fmt"
 
 func main() {
 	var numbers = []int{10, 20, 30, 40}
-	mutateSlice(&numbers, func(i int) int {return i*2})
+	mutateSlice(&numbers,double)
+	fmt.Println(numbers)
+
+	mutateSlice(&numbers,triple)
+
 	fmt.Println(numbers)
 }
 
@@ -13,4 +17,10 @@ func mutateSlice(numbers *[]int,operation func(int) int) {
 	for index, val := range *numbers {
 		(*numbers)[index]=operation(val);
 	}
+}
+func double(number int) int{
+	return number*2;
+}
+func triple(number int) int{
+	return number*3;
 }
