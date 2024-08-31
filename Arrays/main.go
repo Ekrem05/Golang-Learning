574a69a78c333c26403e7a596d8c86a4041c1f38
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-08-31 11:38:39 +0300

    slices

--- Arrays/main.go
+++ Arrays/main.go
@@ -6,7 +6,7 @@ func main() {
 	var products [4]string = [4]string {"Book", "Ball"}
 	products[2]="Toy"
 	fmt.Print(products)
-
+	fmt.Print(products[1:3])
 
 	prices := [4]int{10,30,12,5}
 	fmt.Print(prices)
