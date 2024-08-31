1f8f0874480fced53454566bb4ddd4899cc812f4
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-08-31 14:26:11 +0300

    functions as values

---
+++ Functions-Deep-Dive/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numbers = []int{10, 20, 30, 40}
+	mutateSlice(&numbers,double)
+	fmt.Println(numbers)
+
+	mutateSlice(&numbers,triple)
+
+	fmt.Println(numbers)
+}
+
+func mutateSlice(numbers *[]int,operation func(int) int) {
+
+	for index, val := range *numbers {
+		(*numbers)[index]=operation(val);
+	}
+}
+func double(number int) int{
+	return number*2;
+}
+func triple(number int) int{
+	return number*3;
+}
