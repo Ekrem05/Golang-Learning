b52e1be2d44c14f4b296acc5ebda0832b7fe0774
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-08-31 11:54:48 +0300

    slices,cap(),len()

--- Arrays/main.go
+++ Arrays/main.go
@@ -3,10 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var products [4]string = [4]string {"Book", "Ball"}
+	var products [4]string = [4]string {"Book", "Ball","","Car"}
 	products[2]="Toy"
-	fmt.Print(products)
-	fmt.Print(products[1:3])
+	
+	bestProducts := products[1:2]
+	otherProducts:=bestProducts[:3]	
+	fmt.Print(bestProducts)
+	fmt.Println(otherProducts)
+	fmt.Print(len(bestProducts), cap(bestProducts))
 
 	prices := [4]int{10,30,12,5}
 	fmt.Print(prices)
